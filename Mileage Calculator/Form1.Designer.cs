0abac86f03b7062901d049601648f416b83f7636
wolfshirts <wolfshirts@tanana>
2018-10-26 19:01:43 -0700

    created basic mileage calculator

--- Mileage Calculator/Form1.Designer.cs
+++ Mileage Calculator/Form1.Designer.cs
@@ -28,13 +28,101 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.startingMileageBox = new System.Windows.Forms.TextBox();
+            this.endingMileageBox = new System.Windows.Forms.TextBox();
+            this.mileageRateBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Starting Mileage:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ending Mileage:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 63);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Mileage Rate:";
+            // 
+            // startingMileageBox
+            // 
+            this.startingMileageBox.Location = new System.Drawing.Point(104, 4);
+            this.startingMileageBox.Name = "startingMileageBox";
+            this.startingMileageBox.Size = new System.Drawing.Size(100, 20);
+            this.startingMileageBox.TabIndex = 3;
+            // 
+            // endingMileageBox
+            // 
+            this.endingMileageBox.Location = new System.Drawing.Point(104, 30);
+            this.endingMileageBox.Name = "endingMileageBox";
+            this.endingMileageBox.Size = new System.Drawing.Size(100, 20);
+            this.endingMileageBox.TabIndex = 4;
+            // 
+            // mileageRateBox
+            // 
+            this.mileageRateBox.Location = new System.Drawing.Point(104, 56);
+            this.mileageRateBox.Name = "mileageRateBox";
+            this.mileageRateBox.Size = new System.Drawing.Size(100, 20);
+            this.mileageRateBox.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(129, 95);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(360, 130);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.mileageRateBox);
+            this.Controls.Add(this.endingMileageBox);
+            this.Controls.Add(this.startingMileageBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Mileage Calculator ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox startingMileageBox;
+        private System.Windows.Forms.TextBox endingMileageBox;
+        private System.Windows.Forms.TextBox mileageRateBox;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Mileage Calculator/Form1.cs
+++ Mileage Calculator/Form1.cs
@@ -16,5 +16,65 @@ namespace Mileage_Calculator
         {
             InitializeComponent();
         }
+
+        private bool CheckValues()
+        {
+            UInt64 startMiles;
+            UInt64 endMiles;
+            Double mileageRate;
+            try
+            {
+                startMiles = Convert.ToUInt64(startingMileageBox.Text);
+                endMiles = Convert.ToUInt64(endingMileageBox.Text);
+                mileageRate = Convert.ToDouble(mileageRateBox.Text);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            if(Convert.ToUInt64(startingMileageBox.Text) > Convert.ToUInt64(endingMileageBox.Text))
+            {
+                return false; //We could handle this with a more specific error message.
+            }
+            return true;
+        }
+
+        private Double CalculateValueOwed()
+        {
+            UInt64 startMiles;
+            UInt64 endMiles;
+            Double mileageRate;
+            
+            startMiles = Convert.ToUInt64(startingMileageBox.Text);
+            endMiles = Convert.ToUInt64(endingMileageBox.Text);
+            mileageRate = Convert.ToDouble(mileageRateBox.Text);
+
+            Double owed = (endMiles - startMiles) * mileageRate;
+
+            return owed; //owed is unnecessary, but it's not like i'm short memory here and I think it's clearer.
+        }
+
+        private UInt64 CalculateTotalMilesTravelled()
+        {
+            UInt64 startMiles = Convert.ToUInt64(startingMileageBox.Text);
+            UInt64 endMiles = Convert.ToUInt64(endingMileageBox.Text);
+            return endMiles - startMiles;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!CheckValues())
+            {
+                MessageBox.Show("Invalid values in form.");
+                return;
+            }
+            else
+            {
+                Double cashOwed = CalculateValueOwed();
+                UInt64 milesTravelled = CalculateTotalMilesTravelled();
+                MessageBox.Show("Total cash owed: " + cashOwed + "\n" + "Total miles travelled: " + milesTravelled);
+            }
+
+        }
     }
 }
